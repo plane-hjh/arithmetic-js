e94da0ebbf4edb758b1058f81c629ac1af9ae9b2
plane-hjh <18211495117@163.com>
2020-07-03 10:56:03 +0800

    update treeBigDepth

--- binaryTree/treeBigDepth.js
+++ binaryTree/treeBigDepth.js
@@ -4,5 +4,5 @@
 
 // 一棵二叉树的最大深度等于左子树深度和右子树最大深度的最大值 + 1
 const treeBigDepth = function (root) {
-  return !root ? 0 : Math.max(treeDepth(root.left), treeDepth(root.right)) + 1
+  return !root ? 0 : Math.max(treeBigDepth(root.left), treeBigDepth(root.right)) + 1
 }
